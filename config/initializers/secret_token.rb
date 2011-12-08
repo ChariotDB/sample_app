22c4889ee7b4a8b98207cedab549177545bff989
ChariotDB <pacificdbs@hotmail.com>
2011-12-08 04:40:41 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '16caa24f36a812d7e5774f9dc833a6affeec82bfc26a4bf9f6b9af21c634c74ae2a2b96738ef500a41644355e785e7e7b7b15fdd1dc43080ed00e2fe214c5c5e'
